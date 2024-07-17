fa12eb2c1772ce0d05a51968a9261b87ef4ea27b
Ashik Ahammed Hridoy <35147425+ahammedhridoy@users.noreply.github.com>
2024-07-17 23:30:44 +0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const { contactRouter } = require("./routes/contactRoute");
 const app = express();
 app.use(express.json());
 const corsOptions = {
-  origin: ["https://mern-blog-app-frontend-iv8d00726.vercel.app/"],
+  origin: ["https://mern-blog-app-frontend-iv8d00726.vercel.app"],
   credentials: true,
   optionSuccessStatus: 200,
   allowedHeaders: ["Content-Type", "Authorization"],
